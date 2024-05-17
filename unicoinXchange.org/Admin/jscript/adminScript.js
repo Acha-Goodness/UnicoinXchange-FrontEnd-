13c4ee8b7c6a0c3b12b133e1267050bdca385531
Acha-Goodness <acharious1668@gmail.com>
2024-05-17 21:25:14 +0100

    admin dashboard update

--- unicoinXchange.org/Admin/jscript/adminScript.js
+++ unicoinXchange.org/Admin/jscript/adminScript.js
@@ -259,19 +259,23 @@ if(window.location.pathname === '/unicoinXchange.org/Admin/html/admin.html'){
             const card = document.createElement("div");
             const userIcon = document.createElement("i");
             const name = document.createElement("h5");
+            const role = document.createElement("p");
+            const userId = document.createElement("p")
             const btn1 = document.createElement("button");
             const btn2 = document.createElement("button");
             const btn3 = document.createElement("button");
             const deleteIcon = document.createElement("i");
 
             card.classList.add("user-card");
+            userId.classList.add("userId");
             btn1.classList.add("card-btn");
             btn2.classList.add("card-btn");
             btn3.classList.add("card-btn");
-
             userIcon.classList.add("fa");
             userIcon.classList.add("fa-user");
             name.innerText = user.name;
+            userId.innerText = user._id;
+            role.innerText = user.role;
             btn1.innerText = "Update Client Investment Amount";
             btn2.innerText = "Activate Client Investment";
             btn3.innerText = "De-activate Client Investment";
@@ -280,6 +284,8 @@ if(window.location.pathname === '/unicoinXchange.org/Admin/html/admin.html'){
 
             card.appendChild(userIcon)
             card.appendChild(name)
+            card.appendChild(userId)
+            card.appendChild(role)
             card.appendChild(btn1)
             card.appendChild(btn2)
             card.appendChild(btn3)
@@ -291,11 +297,13 @@ if(window.location.pathname === '/unicoinXchange.org/Admin/html/admin.html'){
         delBtn.addEventListener("click", () => {
           const cards = document.querySelectorAll(".user-card");
           Array.from(cards).map(card => {
-            const btns = card.querySelectorAll(".card-btn")
             const trashBtn = card.querySelector(".fa-trash");
+            const btns = card.querySelectorAll(".card-btn")
+
             trashBtn.classList.toggle("active-trash")
+
             Array.from(btns).map(btn => {
-                btn.classList.toggle(".active-card-btn")
+                btn.classList.toggle("active-card-btn")
             })
           })
 
@@ -305,6 +313,108 @@ if(window.location.pathname === '/unicoinXchange.org/Admin/html/admin.html'){
             delBtn.innerText = "Delete Client"
           }
         })
+
+        const updateAdminDetailsForm = document.querySelector(".update-admin-details-form");
+        const updateAdminPasswordForm = document.querySelector(".update-Admin-password-form");
+
+        updateAdminDetailsForm.style.display = "none";
+        updateAdminPasswordForm.style.display = "none";
+
+        retriveCards(users)
+    };
+
+      // UPDATE CLIENT INVESTMENT AMOUNT
+      const updateClientInvesmentAmtForm = (userId) => {
+
+        const userCardsWrap = document.querySelector(".user-cards-wrap");
+        const updateAdminDetailsForm = document.querySelector(".update-admin-details-form");
+        const adminsCard = document.querySelector(".admins-card");
+        const updateAdminPasswordForm = document.querySelector(".update-Admin-password-form");
+        const UpdateCliInvestForm = document.querySelector(".update-cli-inv-form");
+        const delBtn = document.getElementById("del-client");
+
+        userCardsWrap.style.display = "none";
+        updateAdminDetailsForm.style.display = "none";
+        updateAdminPasswordForm.style.display = "none";
+        adminsCard.style.display = "none";
+        UpdateCliInvestForm.style.display = "block";
+        delBtn.style.display = "none";
+
+        const jwtToken = localStorage.getItem("adminJwtToken")
+
+        UpdateCliInvestForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+        
+            const amount = document.getElementById("ammount");
+            const coin = document.getElementById("coin-type");
+
+            axios.post(`http://127.0.0.1:7000/api/v1/admin/setUserInvestmentAmount/${userId}`,{
+                amount: amount.value.trim(),
+                paymentMode: coin.value.trim(),
+            },{
+                headers: {
+                    "Content-Type" : 'application/json',
+                    "Authorization" : `Bearer ${jwtToken}`
+            }}).then(res => {
+                console.log(res)
+            }).catch(err => {
+                console.log(err)
+            })
+        });
+      };
+
+      const activateClientInvesment = (userId) => {
+        const jwtToken = localStorage.getItem("adminJwtToken")
+        console.log(jwtToken)
+        console.log(userId)
+        axios.post(`http://127.0.0.1:7000/api/v1/admin/activateUserInvestment/${userId}`, {
+            headers: {
+                "Content-Type" : 'application/json',
+                "Authorization" : `Bearer ${jwtToken}`
+            }
+        }).then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
+      };
+
+      const deActivateClientInvesment = (userId) => {
+        const jwtToken = localStorage.getItem("adminJwtToken");
+          
+        axios.post(`http://127.0.0.1:7000/api/v1/admin/deactivateUserInvestment/${userId}`,{
+            headers: {
+                "Content-Type" : 'application/json',
+                "Authorization" : `Bearer ${jwtToken}`
+            }
+        }).then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
+      };
+
+      const  retriveCards = () => {
+        const userCardsWrap = document.querySelector(".user-cards-wrap");
+        if(userCardsWrap.children !== null){
+            const userCards = userCardsWrap.getElementsByClassName("user-card");
+            const cardsArray = Array.from(userCards)
+            cardsArray.map((card) => {
+                card.addEventListener("mouseover", () => {
+                    const cardBtns = card.querySelectorAll(".card-btn"); 
+                    const userId = card.querySelector(".userId").innerText;
+
+                    Array.from(cardBtns).map((cardBtn, idx) => {
+                        cardBtn.addEventListener("click", (e) => {
+                            e.stopImmediatePropagation()
+                            if(idx === 0) updateClientInvesmentAmtForm(userId);
+                            if(idx === 1) activateClientInvesment(userId);
+                            if(idx === 2) deActivateClientInvesment(userId);
+                        })
+                    })
+                })
+            })
+        }
     };
 
     const getAllUser = () => {
@@ -312,11 +422,10 @@ if(window.location.pathname === '/unicoinXchange.org/Admin/html/admin.html'){
         
         axios.get("http://127.0.0.1:7000/api/v1/admin/getAllUsers", {
             headers: {
-                "Content-Type": 'application/json',
+                "Content-Type" : 'application/json',
                 "Authorization" : `Bearer ${jwtToken}`
             }
         }).then(res => {
-            // console.log(res);
             displayUsers(res.data.data.users);
         }).catch(err => {
             console.log(err);
@@ -332,13 +441,177 @@ if(window.location.pathname === '/unicoinXchange.org/Admin/html/admin.html'){
         getAllUser();
     });
 
-// LOGOUT
-   const logoutBtn = document.querySelector(".logout-btn");
+    // GET ALL USERS RELOAD BUTTON
+    const getAllUsersReloadBtn = document.getElementById("get-all-user");
 
-   logoutBtn.addEventListener("click", () => {
-       localStorage.removeItem("adminJwtToken");
-       localStorage.removeItem("adminData");
-       window.location.href =  'adminAuth.html';
-   });
+    getAllUsersReloadBtn.addEventListener("click", () => {
+        window.location.reload();
+    });
 
+    // GET ALL ADMIN
+    const displayAdmin = (admins) => {
+        const adminsCard = document.querySelector(".admins-card");
+
+        Array.from(admins).map(admin => {
+            const adminCard = document.createElement("div");
+            const adminIcon = document.createElement("i");
+            const adminName = document.createElement("h3");
+            const role = document.createElement("p");
+            const delBtn = document.createElement("button");
+
+            adminCard.classList.add("adminCard");
+            adminIcon.classList.add("fa");
+            adminIcon.classList.add("fa-user");
+            adminName.innerText = admin.name;
+            role.innerText = admin.role;
+            delBtn.innerText = "delete Admin";
+
+            adminCard.appendChild(adminIcon);
+            adminCard.appendChild(adminName);
+            adminCard.appendChild(role);
+            adminCard.appendChild(delBtn);
+
+            adminsCard.appendChild(adminCard);
+        })
+
+        // if(admins.length < 2){
+        //     adminsCard.style.
+        // }
+    };
+
+    const getAllAdminBtn = document.getElementById("get-all-admin");
+    const delBtn = document.getElementById("del-client");
+
+    getAllAdminBtn.addEventListener("click", () => {
+        const jwtToken = localStorage.getItem("adminJwtToken");
+
+        axios.get("http://127.0.0.1:7000/api/v1/admin/getAllAdmin",{
+            headers: {
+                "Content-Type": 'application/json',
+                "Authorization" : `Bearer ${jwtToken}`
+            }
+        }).then(res => {
+            displayAdmin(res.data.data.DataTransferItem);
+        }).catch(err => {
+            console.log(err);
+        });
+
+        const userCardsWrap = document.querySelector(".user-cards-wrap");
+        const updateAdminDetailsForm = document.querySelector(".update-admin-details-form");
+        const adminsCard = document.querySelector(".admins-card");
+        const updateAdminPasswordForm = document.querySelector(".update-Admin-password-form");
+        const UpdateCliInvestForm = document.querySelector(".update-cli-inv-form");
+
+        userCardsWrap.style.display = "none";
+        updateAdminDetailsForm.style.display = "none";
+        updateAdminPasswordForm.style.display = "none";
+        UpdateCliInvestForm.style.display = "none";
+
+        getAllAdminBtn.style.display = "none";
+        delBtn.style.display = "none";
+
+        if(adminsCard.style.display === "none") adminsCard.style.display = "flex";
+    });
+
+    // UPDATE ADMIN DETAILS
+    const updateAdminDetailsBtn = document.getElementById("update-admin-details");
+    
+    updateAdminDetailsBtn.addEventListener("click", () => {
+        
+        const adminsCard = document.querySelector(".admins-card");
+        const userCardsWrap = document.querySelector(".user-cards-wrap");
+        const getAllAdminBtn = document.getElementById("get-all-admin");
+        const updateAdminDetailsForm = document.querySelector(".update-admin-details-form");
+        const updateAdminPasswordForm = document.querySelector(".update-Admin-password-form");
+        const UpdateCliInvestForm = document.querySelector(".update-cli-inv-form");
+        const delBtn = document.getElementById("del-client");
+
+        userCardsWrap.style.display = "none";
+        adminsCard.style.display = "none";
+        getAllAdminBtn.style.display = "block";
+        updateAdminPasswordForm.style.display = "none";
+        UpdateCliInvestForm.style.display = "none";
+        delBtn.style.display = "none";
+        updateAdminDetailsForm.style.display = "block";
+
+        const jwtToken = localStorage.getItem("adminJwtToken");
+
+        updateAdminDetailsForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            const fullName = document.getElementById("full-name");
+            const emailAddress = document.getElementById("email-address");
+
+            console.log(fullName.value);
+            console.log(emailAddress.value);
+
+            axios.patch(`http://127.0.0.1:7000/api/v1/admin/`,{
+                    name: fullName.value.trim(),
+                    email: emailAddress.value.trim(),
+                    headers: {
+                        "Content-Type": 'application/json',
+                        "Authorization" : `Bearer ${jwtToken}`
+                    }
+                }).then(res => {
+                    console.log(res)
+                }).catch(err => {
+                    console.log(err)
+                })
+        })
+    })
+
+    // UPDATE ADMIN PASSWORD
+    const updateAdminPassword = document.getElementById("update-admin-password");
+
+    updateAdminPassword.addEventListener("click", () => {
+
+        const adminsCard = document.querySelector(".admins-card");
+        const userCardsWrap = document.querySelector(".user-cards-wrap");
+        const getAllAdminBtn = document.getElementById("get-all-admin");
+        const updateAdminDetailsForm = document.querySelector(".update-admin-details-form");
+        const updateAdminPasswordForm = document.querySelector(".update-Admin-password-form");
+        const UpdateCliInvestForm = document.querySelector(".update-cli-inv-form");
+        const delBtn = document.getElementById("del-client");
+
+        userCardsWrap.style.display = "none";
+        adminsCard.style.display = "none";
+        getAllAdminBtn.style.display = "block";
+        updateAdminDetailsForm.style.display = "none";
+        updateAdminPasswordForm.style.display = "block";
+        UpdateCliInvestForm.style.display = "none"; 
+        delBtn.style.display = "none";
+
+        const jwtToken = localStorage.getItem("adminJwtToken");
+
+        updateAdminPasswordForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            const currentPassword = document.getElementById("current-password");
+            const newPassword = document.getElementById("new-password");
+            const confirmPassword = document.getElementById("confirm-password");
+
+            axios.patch(`http://127.0.0.1:7000/api/v1/admin/adminUpdatePassword`,{
+                    currentPassword: currentPassword.value.trim(),
+                    password: newPassword.value.trim(),
+                    passwordConfirm: confirmPassword.value.trim(),
+                    headers: {
+                        "Content-Type": 'application/json',
+                        "Authorization" : `Bearer ${jwtToken}`
+                    }
+                }).then(res => {
+                    console.log(res)
+                }).catch(err => {
+                    console.log(err)
+                })
+        });
+    });
+
+    // LOGOUT
+    const logoutBtn = document.querySelector(".logout-btn");
+
+    logoutBtn.addEventListener("click", () => {
+        localStorage.removeItem("adminJwtToken");
+        localStorage.removeItem("adminData");
+        window.location.href =  'adminAuth.html';
+    });
 }
